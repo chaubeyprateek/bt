34f58e596b76e9ca679afdf2466f7464eef0b9af
Menno van Grinsven <scanno71@gmail.com>
2017-09-18 22:07:20 +0200

    bt:msm8952: Update to O
    Change-Id: Ia520210625d537c8255ed7012e52f2a5d509204a

---
+++ Android.mk
@@ -0,0 +1,30 @@
+# TODO:  Find a better way to separate build configs for ADP vs non-ADP devices
+ifneq ($(BOARD_IS_AUTOMOTIVE),true)
+  ifneq ($(filter caf-msm8916 caf-msm8952 caf-msm8992 caf-msm8996 caf-msm8998,$(TARGET_QCOM_BLUETOOTH_VARIANT)),)
+    ifeq (caf-msm8994,$(TARGET_QCOM_BLUETOOTH_VARIANT))
+        TARGET_QCOM_BLUETOOTH_VARIANT := caf-msm8992
+    endif
+    include $(call all-named-subdir-makefiles,$(TARGET_QCOM_BLUETOOTH_VARIANT))
+  else
+  ifneq ($(filter msm8960 msm8x27 msm8974 msm8226,$(TARGET_BOARD_PLATFORM)),)
+    include $(call all-named-subdir-makefiles,msm8960)
+  else
+    ifneq ($(filter msm8994 msm8992,$(TARGET_BOARD_PLATFORM)),)
+      include $(call all-named-subdir-makefiles,msm8992)
+    else
+      ifneq ($(filter msm8996,$(TARGET_BOARD_PLATFORM)),)
+        include $(call all-named-subdir-makefiles,msm8996)
+      else
+        ifneq ($(filter msm8909 ,$(TARGET_BOARD_PLATFORM)),)
+          #For msm8909 target
+          include $(call all-named-subdir-makefiles,msm8909)
+        else
+          ifneq ($(filter msm8998,$(TARGET_BOARD_PLATFORM)),)
+            include $(call all-named-subdir-makefiles,msm8998)
+          endif
+        endif
+      endif
+    endif
+  endif
+  endif
+endif
